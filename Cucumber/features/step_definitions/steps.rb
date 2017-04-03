cf6e8d6a22c15da2274faf1fae3f24e755912408
Alex Gómez Uceda <alexyuncos96@gmail.com>
2017-04-03 14:19:46 +0200

    Cucumber folder added.
    Cucumber folder added.

---
+++ Cucumber/features/step_definitions/steps.rb
@@ -0,0 +1,54 @@
+
+Given(/^Yo he visitado Youtube$/) do
+end
+
+When(/^Busco "([^"]*)"$/) do |arg1|
+end
+
+Then(/^Recibo los vídeos sobre "([^"]*)" en Youtube$/) do |arg1|
+end
+
+When(/^Me gusta el contenido de un canal de Youtube$/) do
+end
+
+Then(/^Me suscribo a su canal$/) do
+end
+
+Then(/^Me aparecen sus vídeos en la página principal$/) do
+end
+
+When(/^Veo un vídeo de Youtube$/) do
+end
+
+When(/^Me gusta el vídeo$/) do
+end
+
+Then(/^Pulso el botón de "([^"]*)"$/) do |arg1|
+end
+
+Given(/^No tengo cuenta en Youtube$/) do
+end
+
+When(/^Quiero tener una cuenta en Youtube$/) do
+end
+
+Then(/^Introduzco mis datos en la plataforma$/) do
+end
+
+Then(/^Tengo una nueva cuenta en Youtube$/) do
+end
+
+Given(/^Tengo una cuenta en Youtube$/) do
+end
+
+When(/^Quiero subir un vídeo a la plataforma$/) do
+end
+
+Then(/^Pulso "([^"]*)"$/) do |arg1|
+end
+
+Then(/^Adjunto el vídeo$/) do
+end
+
+Then(/^Subo el vídeo a la plataforma$/) do
+end
